652ef82c17d0b413319b3a8d9360708b57064bec
myyrakle <sssang97@naver.com>
2023-05-14 12:10:18 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { networkInterfaces } = require("os");
+
+let app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/ip", (req, res) => {
+  let network = networkInterfaces();
+
+  res.send(network?.eth0?.[0]?.address);
+});
+
+app.get("/echo/environments", (req, res) => {
+  res.json(process.env);
+});
+
+app.listen(80, () => {});
